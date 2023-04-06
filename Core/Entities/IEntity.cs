b32269228c8f80dff6a13fd9e7aedc9b7a48763b
İlhan Sönmez <ilhan.sonmez@idesse.com>
2023-04-06 14:49:06 +0300

    feat(entities): create entity classes by inheriting from a core entity interface

---
+++ Core/Entities/IEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Core.Entities
+{
+	public class IEntity
+	{
+		public int Id { get; set; }
+	}
+}
---
+++ Entities/Concrete/Account.cs
@@ -0,0 +1,10 @@
+﻿using Core.Entities;
+
+namespace Entities.Concrete
+{
+	public class Account:IEntity
+	{
+		public int OwnerUserId { get; set; }
+		public string AccountName { get; set; }
+	}
+}
